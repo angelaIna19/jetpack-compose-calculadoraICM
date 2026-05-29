90b517796d7cc28ea190cc4850f6397169670c95
angelaIna19 <cristiardi19@gmail.com>
2026-05-29 16:15:10 -0500

    test2

--- app/src/main/java/com/example/jetpack_compose_calculadoraicm/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_calculadoraicm/MainActivity.kt
@@ -102,7 +102,7 @@ fun PantallaCaptura() {
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        // Campo para la Altura gg
+        // Campo para la Altura
         OutlinedTextField(
             value = altura,
             onValueChange = { altura = it },
